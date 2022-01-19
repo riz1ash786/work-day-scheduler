b4ca0e85a2c32e72c868cc28f990382de4b8e222
Rizwan Ashraf <riz1ash786@gmail.com>
2022-01-19 18:13:20 +0000

    attempting for loops for timeblock colours

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,13 @@
-var todayDate = moment().format("dddd, MMM Do YYYY H:mm:ss");
-$("#currentDay").html(todayDate);
+// Display today's day and date
+var currentDate = moment().format("dddd MMM Do YYYY, h:mm a");
+$("#currentDay").append(currentDate);
+
+for (var i = 9; i < 18; i++) {
+  if (i > currentHour) {
+    $("#hour-" + i).addClass("future");
+  } else if (i === currentHour) {
+    $("#hour-" + i).addClass("past");
+  } else if (i < currentHour) {
+    $("#hour-" + i).addClass("past");
+  }
+}
